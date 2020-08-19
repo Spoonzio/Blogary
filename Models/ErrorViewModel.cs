d02434f6b986a67120d88bf981c6cfe554108767
JasonPan <jpan569@gmail.com>
2020-08-18 19:31:48 -0700

    Initial commit (Template)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blogary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
